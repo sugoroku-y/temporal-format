0e44d43158c1810f82e79f6611440e03cf11ee5e
sugoroku-y <yebisuya@gmail.com>
2026-07-09 02:02:30 +0900

    test: errorのテストを追加

--- src/tests/error.test.ts
+++ src/tests/error.test.ts
@@ -1,13 +1,13 @@
 import { error } from '../error';
 
 describe('error', () => {
-    it('1', () => {
+    it('1. throw Error with string', () => {
         expect(() => error('test')).toThrow('test');
     });
-    it('2', () => {
+    it('2. throw Error with template literal', () => {
         expect(() => error`abc${123}def`).toThrow('abc123def');
     });
-    it('3', () => {
+    it('3. rethrow Error', () => {
         expect(() => {
             try {
                 void error`abc: ${new Error('test')}`;
@@ -18,5 +18,20 @@ describe('error', () => {
                 throw ex;
             }
         }).toThrow('abc: test');
+    });
+    it('4. throw Error with a truncated stacktrace', () => {
+        const a = () => error`test${c}`;
+        const b = () => a();
+        const c = () => b();
+        const d = () => c();
+        try {
+            d();
+            expect.fail();
+        } catch (ex) {
+            assert(ex instanceof Error);
+            expect(ex.stack).toEqual(
+                expect.stringMatching(/^Error: test\n *at d \(/),
+            );
+        }
     });
 });
