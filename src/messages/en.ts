662a5da0ad9acb5efb1e1c38376d13e4fb4c0d88
sugoroku-y <yebisuya@gmail.com>
2026-08-17 00:43:18 +0900

    fix: メッセージを改善

--- src/messages/en.ts
+++ src/messages/en.ts
@@ -1,23 +1,39 @@
 import ja from './ja';
 
 const messages = {
+    /** Error when an unsupported locale name is specified */
     unsupportedLocale: 'Unsupported locale: ${locale}',
-    unsupportedCalendarId: 'Unsupported calendar: ${calendarId}',
+    /** Error when an unsupported calendar ID is specified */
+    unsupportedCalendarId: 'Unsupported calendar ID: ${calendarId}',
+    /** Error when monthCode has an unexpected value */
     invalidMonthCode: 'Unexpected monthCode: ${monthCode}',
+    /** Error when an unsupported overflow behavior is specified */
     unsupportedOverflow: 'Unsupported overflow behavior: ${overflow}',
-    unclosedQuote: 'The quote ${quote} is not closed',
-    independentQuote: 'An alone quote ${quote} is used',
-    noFormatToken: 'There is no format token',
+    /** Error when a quote is not closed */
+    unclosedQuote: 'Unclosed quote: ${quote}',
+    /** Error when a quote is used alone */
+    independentQuote: 'Standalone quote used: ${quote}',
+    /** Error when there is no format token */
+    noFormatToken: 'No format token present',
+    /** Error when an invalid format token is present */
     invalidFormatToken: 'Invalid format token: ${token}',
+    /** Error when a property does not exist on the target instance */
     noProperty: '${instance} does not have property ${property}: ${token}',
+    /** Error when a method does not exist on the target instance */
     noMethod: '${instance} does not have method ${method}',
-    noDateTimeToken: 'There is no date or time format token',
+
+    /** Error when there is no date or time format token */
+    noDateTimeToken: 'No date or time format token present',
+    /** Error when AM/PM is used but 12-hour token is missing */
     required12hoursWhenUsingAmPm:
-        'When using AM/PM token (a), 12-hour token (h/hh) is also required',
+        'When using AM/PM token (a), a 12-hour token (h/hh) is required',
+    /** Error when both 12-hour and 24-hour tokens are used */
     dontUseBoth12hoursAnd24Hours:
-        'You cannot specify both 12-hour token (h/hh) and 24-hour token (H/HH)',
+        'Cannot specify both 12-hour token (h/hh) and 24-hour token (H/HH)',
+    /** Error when 12-hour token is used but AM/PM token is missing */
     requiredAmPmWhenUsing12hours:
-        'When using 12-hour token (h/hh), AM/PM token (a) is also required',
+        'When using 12-hour token (h/hh), AM/PM token (a) is required',
+    /** Error when a format token is duplicated */
     duplicateFormatToken: 'Duplicate format token: ${token}',
 } as const satisfies Record<keyof typeof ja, string>;
 
--- src/messages/ja.ts
+++ src/messages/ja.ts
@@ -1,23 +1,38 @@
 const messages = {
-    /** サポートしていないロケール */
-    unsupportedLocale: 'サポートしていないロケール: ${locale}',
-    unsupportedCalendarId: '対応していないカレンダーです: ${calendarId}',
-    invalidMonthCode: '想定外のmonthCodeです: ${monthCode}',
-    unsupportedOverflow: 'サポートしていないオーバーフローの挙動: ${overflow}',
+    /** サポートされていないロケール名が指定されたときのエラー */
+    unsupportedLocale: 'サポートされていないロケール: ${locale}',
+    /** サポートされていないカレンダーIDが指定されたときのエラー */
+    unsupportedCalendarId: 'サポートされていないカレンダーID: ${calendarId}',
+    /** monthCodeの値が想定外だったときのエラー */
+    invalidMonthCode: '想定外のmonthCode: ${monthCode}',
+    /** サポートされていないoverflowの挙動が指定されたときのエラー */
+    unsupportedOverflow:
+        'サポートされていないオーバーフローの挙動: ${overflow}',
+    /** 対象インスタンスに指定したプロパティが存在しないときのエラー */
     noProperty: '${instance}にはプロパティ${property}がありません: ${token}',
+    /** 対象インスタンスに指定したメソッドが存在しないときのエラー */
     noMethod: '${instance}にはメソッド${method}がありません',
 
+    /** 引用符が閉じられていないときのエラー */
     unclosedQuote: '引用符${quote}が閉じられていません',
-    independentQuote: '単独の引用符${quote}が使われています',
-    noFormatToken: '書式指定子がありません',
-    invalidFormatToken: '無効な書式指定子です: ${token}',
-    noDateTimeToken: '日付や時刻の書式指定子がありません',
+    /** 引用符が単独で使用されているときのエラー */
+    independentQuote: '引用符${quote}が単独で使用されています',
+    /** 書式指定子がないときのエラー */
+    noFormatToken: '書式指定子が見つかりません',
+    /** 無効な書式指定子が含まれているときのエラー */
+    invalidFormatToken: '無効な書式指定子: ${token}',
+    /** 日付または時刻に対応する書式指定子がないときのエラー */
+    noDateTimeToken: '日付または時刻の書式指定子がありません',
+    /** 午前/午後を使う場合に12時間表記が不足しているときのエラー */
     required12hoursWhenUsingAmPm:
-        '午前/午後(a)がある場合、12時間表記(h/hh)も必要です',
+        '午前/午後(a)を使う場合、12時間表記(h/hh)が必要です',
+    /** 12時間表記と24時間表記を同時に使ったときのエラー */
     dontUseBoth12hoursAnd24Hours:
-        '12時間表記(h/hh)と24時間表記(H/HH)の両方を指定することはできません',
+        '12時間表記(h/hh)と24時間表記(H/HH)を同時に指定することはできません',
+    /** 12時間表記を使う場合に午前/午後が不足しているときのエラー */
     requiredAmPmWhenUsing12hours:
-        '12時間表記(h/hh)がある場合、午前/午後(a)も必要です',
+        '12時間表記(h/hh)を使う場合、午前/午後(a)が必要です',
+    /** 書式指定子が重複しているときのエラー */
     duplicateFormatToken: '書式指定子が重複しています: ${token}',
 } as const;
 
